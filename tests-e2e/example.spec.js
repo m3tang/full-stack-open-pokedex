2e06b51346b1c539ce263cf849beda854fa65729
Mitchell Tang <m3tang@gmail.com>
2025-04-14 18:13:11 -0700

    quick update

--- tests-e2e/example.spec.js
+++ tests-e2e/example.spec.js
@@ -1,7 +1,7 @@
 const { test, describe, expect } = require("@playwright/test");
 
 describe("Pokedex", () => {
-  test("front page can be opened", async ({ page }) => {
+  test("front page can be opened and text found", async ({ page }) => {
     await page.goto("/");
     await expect(page.getByText("ivysaur")).toBeVisible();
     await expect(
